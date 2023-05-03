d2735804f9eb7afd9dee0bbc43b5a06b2530d7f8
Prasanth Gondi <gprasanthchowdary@gmail.com>
2023-05-03 17:19:19 +0530

    Resume Link Changed

--- src/App.jsx
+++ src/App.jsx
@@ -117,7 +117,7 @@ function App() {
             <BsGithub className="text-white h-8 w-8 hover:h-9 hover:w-9 lg:h-8 lg:w-8 md:w-7 md:h-7 xs:w-6 xs:h-6" />
           </a>
           <a
-            href="https://drive.google.com/file/d/1U41J1KPHVTk69pvlKqilVdghZmS90_Nb/view?usp=sharing"
+            href="https://drive.google.com/file/d/1IbT1zprDnpauZQkKEqojeMN5I9AaEI1r/view?usp=sharing"
             target="_blank"
             className="h-9 w-9"
           >
